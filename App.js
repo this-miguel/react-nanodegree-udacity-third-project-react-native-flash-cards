c90d1ad3dd85c54917e360ca4c087d86fc809d5a
Miguel Salas <miguel@kundigo.pro>
2017-10-25 00:21:12 -0400

    feat: creates a new api action to update a deck, this returns  the updated deck.  * also does refactorization, the api file to exports a single  object containing all the api actions.

--- App.js
+++ App.js
@@ -1,33 +1,42 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { getDeck, getDecks, initializeDeckLibrary, newDeck} from './utils/api'
+import api from './utils/api'
 
 export default class App extends React.Component {
 
   state = {
     data: 'no data yet',
-    deck: 'no deck yet'
+    deck: 'no deck yet',
   };
 
   componentWillMount(){
-    const testData = {helloDeck: {title: 'helloDeck'}} ;
-    initializeDeckLibrary(testData)
-    newDeck('newDeck');
+    const testData = {
+      helloDeck: {title: 'helloDeck'},
+      textDeck: {title: 'testDeck'},
+    } ;
+    api.initializeDeckLibrary(testData);
+    api.newDeck('newDeck');
   }
 
   componentDidMount(){
-    getDecks()
-      .then(data => this.setState({data: data}));
-    getDeck('newDeck')
+    api.getDeck('newDeck')
       .then(data => this.setState({deck: data}))
+    const testData = {
+      title: 'testDeck',
+      data: 'some data'
+    };
+    api.updateDeck('testDeck', testData);
+    api.getDecks()
+      .then(data => this.setState({data: data}));
   }
+
 
   render() {
     const {data, deck} = this.state;
     return (
       <View style={styles.container}>
-        <Text>decks: {JSON.stringify(data)}</Text>
-        <Text> a particular deck:  {JSON.stringify(deck)}</Text>
+        <Text>decks: {JSON.stringify(data, null, 2)}</Text>
+        <Text> a particular deck:  {JSON.stringify(deck, null, 2)}</Text>
       </View>
     );
   }
--- utils/api.js
+++ utils/api.js
@@ -1,22 +1,32 @@
 import { AsyncStorage } from 'react-native'
 export const DECK_LIBRARY = 'MobileFlashcards:DeckLibrary';
 
-export function initializeDeckLibrary(data){
-  AsyncStorage.setItem(DECK_LIBRARY, JSON.stringify(data))
-}
+const api = {
+  initializeDeckLibrary: function (data){
+    AsyncStorage.setItem(DECK_LIBRARY, JSON.stringify(data))
+  },
+  getDecks: function (){
+    return AsyncStorage.getItem(DECK_LIBRARY)
+      .then((data) => JSON.parse(data))
+  },
+  getDeck: function (key){
+    return this.getDecks()
+      .then(data => data[key])
+  },
+  newDeck: function (title) {
+    return AsyncStorage.mergeItem(DECK_LIBRARY, JSON.stringify({
+      [title]: {}
+    }))
+  },
+  updateDeck: function (key, data)  {
+    // As this merges the data inside the DECK_LIBRARY, this will also creates the deck if it does not exist.
+    return AsyncStorage.mergeItem(DECK_LIBRARY, JSON.stringify({
+      [key]: data
+    })).then( () => (
+      // This part is just to return the updated deck.
+      this.getDeck(key)
+    ))
+  }
+};
 
-export function getDecks(){
-  return AsyncStorage.getItem(DECK_LIBRARY)
-    .then((data) => JSON.parse(data))
-}
-
-export function getDeck(key){
-  return getDecks()
-    .then(data => data[key])
-}
-
-export function newDeck(title) {
-  return AsyncStorage.mergeItem(DECK_LIBRARY, JSON.stringify({
-    [title]: {}
-  }))
-}
+export default api;
