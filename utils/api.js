760416f0514b336cad4870141df87135aad99ea8
Miguel Salas <miguel@kundigo.pro>
2017-10-25 00:41:03 -0400

    fix: minor changes using ES6 features to make the code a bit more readable.

--- utils/api.js
+++ utils/api.js
@@ -5,24 +5,28 @@ const api = {
   initializeDeckLibrary: function (data){
     AsyncStorage.setItem(DECK_LIBRARY, JSON.stringify(data))
   },
-  getDecks: function (){
-    return AsyncStorage.getItem(DECK_LIBRARY)
+
+  getDecks: () => (
+    AsyncStorage.getItem(DECK_LIBRARY)
       .then((data) => JSON.parse(data))
-  },
-  getDeck: function (key){
+  ),
+
+  getDeck: function(key) {
     return this.getDecks()
       .then(data => data[key])
   },
-  newDeck: function (title) {
-    return AsyncStorage.mergeItem(DECK_LIBRARY, JSON.stringify({
+
+  newDeck: (title) => (
+    AsyncStorage.mergeItem(DECK_LIBRARY, JSON.stringify({
       [title]: {}
     }))
-  },
-  updateDeck: function (key, data)  {
+  ),
+
+  updateDeck: function (key, data) {
     // As this merges the data inside the DECK_LIBRARY, this will also creates the deck if it does not exist.
     return AsyncStorage.mergeItem(DECK_LIBRARY, JSON.stringify({
       [key]: data
-    })).then( () => (
+    })).then(() => (
       // This part is just to return the updated deck.
       this.getDeck(key)
     ))
