4af2b0b480384f361f0e74002d78b11800e28282
Miguel Salas <miguel@kundigo.pro>
2017-11-04 12:26:17 -0400

    fix: styles the deck menu component.

--- components/DeckMenu.js
+++ components/DeckMenu.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import  { connect } from 'react-redux'
-import { Text, View, TouchableOpacity } from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 import help from '../utils/helpers';
 
 import {
@@ -22,31 +22,39 @@ class DeckMenuDisconnected extends Component {
     const hasCards = questions.length !== 0;
     const { navigate } =  navigation;
     const { replaceWhiteSpaces } = help;
+    const numberOfCards = deck.questions.length;
 
     return(
-      <View>
+      <View style={styles.container}>
+        <View style={[styles.card, styles.cardDimensions]}>
         <Text>{deck.title}</Text>
-        <Text>cards {deck.questions.length}</Text>
+        <Text>{numberOfCards === 1 ? '1 card' : `${numberOfCards} cards`}</Text>
+      </View>
 
-        {
-          hasCards &&
+        <View style={{flexDirection: 'row', alignItems: 'center', justifyContent: "center"}}>
+          {
+            hasCards &&
+            <TouchableOpacity
+              onPress={() => navigate('DeckQuiz', {deckKey: replaceWhiteSpaces(deck.title) })}
+              style={{flex:1}}
+            >
+              <View>
+                <Text style={[styles.button, {backgroundColor: '#96ceb4', color: '#588c7e'}]}>Quiz</Text>
+              </View>
+            </TouchableOpacity>
+          }
+
           <TouchableOpacity
-            onPress={() => navigate('DeckQuiz', {deckKey: replaceWhiteSpaces(deck.title) })}
+            onPress={() => navigate('AddCard', {deckKey: replaceWhiteSpaces(deck.title) })}
+            style={{flex:1}}
           >
-            <View style={{borderRadius: 6, backgroundColor: 'gray'}}>
-              <Text>Quiz</Text>
+            <View >
+              <Text style={[styles.button]}>Add Card</Text>
             </View>
           </TouchableOpacity>
-        }
+        </View>
 
-        <TouchableOpacity
-          onPress={() => navigate('AddCard', {deckKey: replaceWhiteSpaces(deck.title) })}
-        >
-          <View style={{borderRadius: 6, backgroundColor: 'gray'}}>
-            <Text>Add Card</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
+        </View>
     )
   }
 
@@ -76,3 +84,54 @@ const DeckMenu = connect(
   mapDispatchToProps
 )(DeckMenuDisconnected);
 export default DeckMenu;
+
+const styles = StyleSheet.create({
+  container: {
+
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#989898'
+
+  },
+  card: {
+
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#d8dfe5',
+
+  },
+  cardDimensions: {
+
+    width: 260,
+    height: 260,
+    borderRadius: 4,
+
+  },
+  cardText: {
+
+    fontSize: 16,
+    color: 'black',
+    fontWeight: '300',
+    padding: 20,
+    borderRadius: 4,
+    fontFamily: 'Roboto',
+    textAlign: 'center',
+
+  },
+  button: {
+
+    textAlign: 'center',
+    backgroundColor: '#1a75ff',
+    color: '#80b3ff',
+    marginTop: 60,
+    borderRadius: 4,
+    padding: 6,
+    marginRight: 8,
+    marginLeft: 8,
+    fontSize: 12,
+    fontWeight: '600',
+    fontFamily: 'monospace',
+
+  }
+});
