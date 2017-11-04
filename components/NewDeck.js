6d112031a9aeb609d37443050dfee1fab01b30d4
Miguel Salas <miguel@kundigo.pro>
2017-11-04 12:46:08 -0400

    fix: styles the new deck component.

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import  { connect } from 'react-redux'
 import help from '../utils/helpers'
-import { Text, TextInput, View, TouchableOpacity } from 'react-native';
+import { Text, TextInput, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 import {
   asyncNewDeck,
@@ -34,21 +34,24 @@ class NewDeckDisconnected extends Component {
     const { text } = this.state;
     const {replaceWhiteSpaces} = help;
     const validDeckName = replaceWhiteSpaces(text).length !== 0    ;
-
+    const {container, card, cardDimensions, cardText, button } = styles;
     return(
-      <View>
-        <Text>Please enter the name of the new deck below</Text>
-        <TextInput
-          onChangeText={(text) => this.setState({text})}
-          value={text}
-        />
+      <View style={container}>
+        <View style={[card, cardDimensions]}>
+          <Text style={cardText}>Please enter the name of the new deck below:</Text>
+          <TextInput
+            style={[ cardText, {width: 200} ]}
+            onChangeText={(text) => this.setState({text})}
+            value={text}
+          />
+        </View>
         {
           validDeckName &&
           <TouchableOpacity
             onPress={this.handleSubmit}
           >
-            <Text>
-              Submit
+            <Text style={button}>
+              Save
             </Text>
           </TouchableOpacity>
         }
@@ -76,3 +79,54 @@ const NewDeck = connect(
   mapDispatchToProps
 )(NewDeckDisconnected);
 export default NewDeck;
+
+const styles = StyleSheet.create({
+  container: {
+
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: '#989898',
+
+  },
+  card: {
+
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#d8dfe5',
+
+  },
+  cardDimensions: {
+
+    width: 260,
+    height: 260,
+    borderRadius: 4,
+
+  },
+  cardText: {
+
+    fontSize: 16,
+    color: 'black',
+    fontWeight: '300',
+    padding: 20,
+    borderRadius: 4,
+    fontFamily: 'Roboto',
+    textAlign: 'center',
+
+  },
+  button: {
+
+    textAlign: 'center',
+    backgroundColor: '#1a75ff',
+    color: '#80b3ff',
+    marginTop: 30,
+    borderRadius: 4,
+    padding: 6,
+    marginRight: 8,
+    marginLeft: 8,
+    fontSize: 12,
+    fontWeight: '600',
+    fontFamily: 'monospace',
+
+  }
+});
